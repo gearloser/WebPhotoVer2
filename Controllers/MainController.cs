5e6483c4af17481c864681c2e396feb11bee5ea1
DESKTOP-915DLHG\j <kovalevskij01@mail.ru>
2023-01-13 16:53:35 +0300

    Добавьте файлы проекта.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPhotosVer2.Controllers;
+
+namespace WebPhotosVer2.Controllers
+{
+    [Route("main")]
+    public class MainController:Controller
+    {
+        [HttpGet]
+        [Route("test")]
+        public void Test()
+        {
+            int i = 0;
+            i++;
+        }
+    }
+}
